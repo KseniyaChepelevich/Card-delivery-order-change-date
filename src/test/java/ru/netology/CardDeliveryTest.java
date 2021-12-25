5b1d94d822e246efdf68772e0982575492b00130
KseniyaChepelevich <chepelevich.qamid.5@gmail.com>
2021-12-25 11:00:57 +0300

    updated DataGenerator

--- src/test/java/ru/netology/CardDeliveryTest.java
+++ src/test/java/ru/netology/CardDeliveryTest.java
@@ -1,5 +1,6 @@
 package ru.netology;
 
+import com.codeborne.selenide.Configuration;
 import com.github.javafaker.Faker;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
@@ -22,6 +23,13 @@ import ru.netology.data.RegistrationInfo;
 
 public class CardDeliveryTest {
 
+    RegistrationInfo info = DataGenerator.Registration.generateInfo("ru");
+
+
+    //String sPhoneNumber = String.valueOf(Long.parseLong(info.getPhoneNumber()).replaceAll("\\D+", ""));
+
+    //String sPhoneNumber = String.format(info.getPhoneNumber() ).
+
 
 
         public String deleteString = Keys.chord(Keys.CONTROL, "a") + Keys.DELETE;
@@ -32,29 +40,29 @@ public class CardDeliveryTest {
 
         @Test
         public void shouldSendCompletedForm() {
-            open("http://localhost:9999");
-            RegistrationInfo info = DataGenerator
-                    .Registration
-                    .generateInfo("ru");
+            Configuration.holdBrowserOpen = true;
+
+            //String planningDate = generateDate(3);
+            open("http://localhost:9999"); //Открыть приложение
+            $("[data-test-id=\"city\"] .input__control").setValue(info.getAddress()); //Заполнить поле Город
+            $("[data-test-id='date'] .input__control").sendKeys(deleteString);//Заполнить поле Дата встречи спредставителем банка
+            $("[data-test-id='date'] .input__control").setValue(info.getDate());
+            $x("//*[@name=\"name\"]").setValue(info.getName()); //Заполнить поле Фамилия Имя
+            $x("//*[@name=\"phone\"]").setValue("+" + info.getPhoneNumber());//Заполнить поле Мобильный телефон
+            $("[data-test-id=\"agreement\"] .checkbox__box").click();//Кликнуть чекбокс
+            $(".button__text").click();//Нажать кнопку Забронировать
+            //Подождать 15 сек
+            $("[data-test-id=\"notification\"]").shouldHave(text("Встреча успешно забронирована на " + info.getDate()), Duration.ofSeconds(15));
+            //Получить всплывающее окно с сообщением "Успешно! Встреча успешно забранирована на
+            $(".notification__content").shouldBe(visible)
+                    .shouldHave(exactText("Встреча успешно забронирована на " + info.getDate()));
+
             System.out.println(info);
 
-//            String planningDate = generateDate(3);
-//            open("http://localhost:9999"); //Открыть приложение
-//            $("[data-test-id=\"city\"] .input__control").setValue("Москва"); //Заполнить поле Город
-//            $("[data-test-id='date'] .input__control").sendKeys(deleteString);//Заполнить поле Дата встречи спредставителем банка
-//            $("[data-test-id='date'] .input__control").setValue(planningDate);
-//            $x("//*[@name=\"name\"]").setValue("Иванов Василий"); //Заполнить поле Фамилия Имя
-//            $x("//*[@name=\"phone\"]").setValue("+79998885566");//Заполнить поле Мобильный телефон
-//            $("[data-test-id=\"agreement\"] .checkbox__box").click();//Кликнуть чекбокс
-//            $(".button__text").click();//Нажать кнопку Забронировать
-//            //Подождать 15 сек
-//            $("[data-test-id=\"notification\"]").shouldHave(text("Встреча успешно забронирована на " + planningDate), Duration.ofSeconds(15));
-//            //Получить всплывающее окно с сообщением "Успешно! Встреча успешно забранирована на
-//            $(".notification__content").shouldBe(visible)
-//                    .shouldHave(exactText("Встреча успешно забронирована на " + planningDate));
-//
 
         }
 
 
-    }
+
+
+}
--- src/test/java/ru/netology/data/DataGenerator.java
+++ src/test/java/ru/netology/data/DataGenerator.java
@@ -1,17 +1,36 @@
 package ru.netology.data;
 
 import com.github.javafaker.Faker;
+import com.github.javafaker.PhoneNumber;
+import lombok.experimental.UtilityClass;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.Locale;
 
+@UtilityClass
+
 public class DataGenerator {
+
+    //Faker.numerify('#########')
+    //Faker::PhoneNumber.cell_phone.gsub(/-/, '')
+
+    @UtilityClass
     public static class Registration {
         public static RegistrationInfo generateInfo(String locale) {
-            Faker faker = new Faker(new Locale(locale));
+            Faker faker = new Faker(new Locale("ru"));
             return new RegistrationInfo(faker.address().city(),
-                    faker.date().toString(),
+                    generateDate(3),
                     faker.name().fullName(),
-                    faker.phoneNumber().phoneNumber());
+                    faker.phoneNumber().subscriberNumber(11));
         }
     }
+
+    public static String generateDate(int days) {
+        return LocalDate.now().plusDays(days).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
+
+    }
+
+   //
+
 }
